7ce2c8c842783a5e20c5e1b7efbace76671d40b0
mtorrisi <mario.torrisi@ingv.it>
2026-05-08 14:11:46 +0200

    fix(assets): use React Leaflet version compatible with InvenioRDM v13
    Downgrade react-leaflet from v4 to v2.8 and remove the direct
    @react-leaflet/core dependency.
    
    InvenioRDM v13 uses React 16, while react-leaflet v4 pulls dependencies
    that require newer React versions. This caused npm dependency resolution
    errors during asset installation.

--- site/oedatarep_invenio_rdm/webpack.py
+++ site/oedatarep_invenio_rdm/webpack.py
@@ -14,8 +14,7 @@ theme = WebpackThemeBundle(
             },
             dependencies={
                 "leaflet": "^1.8.0",
-                "react-leaflet": "^4.0.0",
-                "@react-leaflet/core": "^2.0.0",
+                "react-leaflet": "^2.8.0",
                 "plotly.js-basic-dist-min": "^2.26.1",
                 "react-plotly.js": "^2.6.0",
             },
